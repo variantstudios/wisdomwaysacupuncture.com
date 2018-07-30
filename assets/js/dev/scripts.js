157f3f7801560a6fd7979fc1254998db11c1abf8
Brent Robbins <brent@variantstudios.com>
2018-07-30 00:04:02 -0600

    Added the social share links/buttons to the blog post page. Added the social icons to the footer added associations logos to the footer.

--- assets/js/dev/scripts.js
+++ assets/js/dev/scripts.js
@@ -23,4 +23,38 @@ $(document).ready(function() {
       return window.pageYOffset || document.documentElement.scrollTop;
     }
   });
+
+  // SOCIAL SHARE
+  $('.share-button').click(function(e) {
+    e.preventDefault();
+    window.open(
+      $(this).attr('href'),
+      'fbShareWindow',
+      'height=450, width=550, top=' +
+      ($(window).height() / 2 - 275) +
+      ', left=' +
+      ($(window).width() / 2 - 225) +
+      ', toolbar=0, location=0, menubar=0, directories=0, scrollbars=0'
+    );
+    return false;
+  });
+
+  // GOOGLE ANALYTICS - TRACK SOCIAL SHARE
+  var ga = window[window['GoogleAnalyticsObject'] || 'ga'];
+  if (typeof ga == 'function') {
+
+    var buttons = document.getElementsByClassName('share-button');
+
+    for (var i = 0; i < buttons.length; i++) {
+      buttons[i].addEventListener('click', function handleOutboundLinkClicks(event) {
+        ga('send', 'event', {
+          eventCategory: 'Outbound Link',
+          eventAction: 'click',
+          eventLabel: event.target.href,
+          transport: 'beacon'
+        });
+      });
+    }
+  }
+
 });
